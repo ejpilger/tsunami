6fc491faea573dbf47e006ac3bd54eef21e132dd
Eric Pilger <ejpilger@gmail.com>
2021-02-15 08:51:51 -1000

    Enhancements

--- source/programs/agents/agent_gather.cpp
+++ source/programs/agents/agent_gather.cpp
@@ -1,8 +1,7 @@
 #include "support/configCosmos.h"
 #include "agent/agentclass.h"
 #include "support/jsonclass.h"
-
-#define GPSPORT 10000
+#include "support/jsonobject.h"
 
 
 static Agent *agent;
@@ -17,45 +16,56 @@ static int32_t mmsi;
 int main(int argc, char *argv[])
 {
     int32_t iretn = 0;
-    double utcgga = 0.;
-    double utcgst = 0.;
-    double lat;
-    char latc;
-    double lon;
-    char lonc;
-    uint16_t gpsqi;
-    uint16_t svnum;
-    double hdop;
-    double height;
-    char heightu;
-    double geoidsep;
-    char geoidsepc;
-    double age;
-    double refid;
-    uint16_t csum;
-    char tid;
-    double rms;
-    double errormaj;
-    double errormin;
-    double erroratt;
-    double latsig;
-    double lonsig;
-    double heightsig;
+    struct gpsstruc
+    {
+        double utcgga = 0.;
+        double utcgst = 0.;
+        double lat;
+        char latc;
+        double lon;
+        char lonc;
+        uint16_t gpsqi;
+        uint16_t svnum;
+        double hdop;
+        double height;
+        char heightu;
+        double geoidsep;
+        char geoidsepc;
+        double age;
+        double refid;
+        uint16_t csum;
+        char tid;
+        double rms;
+        double errormaj;
+        double errormin;
+        double erroratt;
+        double latsig;
+        double lonsig;
+        double heightsig;
+    };
+
+    deque <gpsstruc> gpsdata;
 
     // Load configuration file
-//    ifstream cs(data_name_path(agent->nodeName, "", "") + "config.json");
-    ifstream cs(string(argv[1]) + "/config.json");
+    string nodes = get_cosmosnodes();
+    if (nodes.empty())
+    {
+        printf("No ~/cosmos/nodes folder\n");
+        exit (NODE_ERROR_ROOTDIR);
+    }
+
+    ifstream cs(nodes + "/config.json");
     string jstr;
     if (!cs.is_open())
     {
         printf("Could not open configuration file: %d\n", iretn);
-        exit (1);
+        exit (iretn);
     }
 
     if (!getline(cs, jstr))
     {
         printf("Could not read configuration file: %d\n", -errno);
-        exit (1);
+        exit (-errno);
     }
 
     // Extract configuration values from loaded JSON string
@@ -97,65 +107,135 @@ int main(int argc, char *argv[])
 
     string message;
     uint8_t ccsum;
+    gpsstruc nextgps;
+    ElapsedTime et;
+    string gstpath;
+    double gstmjd = currentmjd();
+    string ggapath;
+    double ggamjd = currentmjd();
     while (agent->running())
     {
-        // Collect next GPS message
+        // Collect next set of GPS messages
         iretn = socket_recvfrom(rcvchan, message, 200);
         if (iretn > 0)
-        {printf("valid message received\n%s\n", message.c_str());
+        {
+            printf("valid message received\n%s\n", message.c_str());
             // Valid message received
             if (message.find("GGA") != string::npos)
-            {printf("GGA Message\n");
+            {
+                ggapath = log_write(agent->nodeName, agent->agentName, ggamjd, "", "gga", message);
+                printf("GGA Message\n");
                 iretn = sscanf(message.c_str(), "$GPGGA,%lf,%lf,%c,%lf,%c,%hu,%hu,%lf,%lf,%c,%lf,%c,%lf,%lf*%hu",
-                               &utcgga,
-                               &lat,
-                               &latc,
-                               &lon,
-                               &lonc,
-                               &gpsqi,
-                               &svnum,
-                               &hdop,
-                               &height,
-                               &heightu,
-                               &geoidsep,
-                               &geoidsepc,
-                               &age,
-                               &refid,
-                               &csum);
+                               &nextgps.utcgga,
+                               &nextgps.lat,
+                               &nextgps.latc,
+                               &nextgps.lon,
+                               &nextgps.lonc,
+                               &nextgps.gpsqi,
+                               &nextgps.svnum,
+                               &nextgps.hdop,
+                               &nextgps.height,
+                               &nextgps.heightu,
+                               &nextgps.geoidsep,
+                               &nextgps.geoidsepc,
+                               &nextgps.age,
+                               &nextgps.refid,
+                               &nextgps.csum);
                 ccsum = 0;
                 for (uint16_t i=1; i<message.size()-3; ++i)
                 {
                     ccsum ^= message[i];
                 }
-                if (csum != ccsum)
+                if (nextgps.csum != ccsum)
                 {
-                    utcgga = 0.;
+                    nextgps.utcgga = 0.;
                 }
             }
 
             if (message.find("GST") != string::npos)
-            {printf("GST Message\n");
+            {
+                log_write(agent->nodeName, agent->agentName, gstmjd, "", "gst", message);
+
+                printf("GST Message\n");
                 iretn = sscanf(message.c_str(), "$G%cGST,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf*%hu",
-                               &tid,
-                               &utcgst,
-                               &rms,
-                               &errormaj,
-                               &errormin,
-                               &erroratt,
-                               &latsig,
-                               &lonsig,
-                               &heightsig,
-                               &csum);
+                               &nextgps.tid,
+                               &nextgps.utcgst,
+                               &nextgps.rms,
+                               &nextgps.errormaj,
+                               &nextgps.errormin,
+                               &nextgps.erroratt,
+                               &nextgps.latsig,
+                               &nextgps.lonsig,
+                               &nextgps.heightsig,
+                               &nextgps.csum);
                 ccsum = 0;
                 for (uint16_t i=1; i<message.size()-3; ++i)
                 {
                     ccsum ^= message[i];
                 }
-                if (csum != ccsum)
+                if (nextgps.csum != ccsum)
                 {
-                    utcgst = 0.;
+                    nextgps.utcgst = 0.;
                 }
             }
+
+            // If we have collected both records then push it to the FIFO
+            if (nextgps.utcgga > 0. && nextgps.utcgst > 0.)
+            {
+                gpsdata.push_back(nextgps);
+                if (gpsdata.size() > 120)
+                {
+                    gpsdata.pop_front();
+                }
+                nextgps.utcgga = 0.;
+                nextgps.utcgst = 0.;
+            }
+        }
+
+        // Every two minutes, average whatever is in the FIFO
+        if (et.split() > 600.)
+        {
+            gpsstruc tempgps;
+            if (gpsdata.size())
+            {
+                for (gpsstruc gps : gpsdata)
+                {
+                    tempgps.lat += gps.lat;
+                    tempgps.lon += gps.lon;
+                    tempgps.height += gps.height;
+                    tempgps.latsig += gps.latsig * gps.latsig;
+                    tempgps.lonsig += gps.lonsig * gps.lonsig;
+                    tempgps.heightsig += gps.heightsig * gps.heightsig;
+                    tempgps.utcgga += gps.utcgga;
+                    tempgps.utcgst += gps.utcgst;
+                }
+                tempgps.lat /= gpsdata.size();
+                tempgps.lon /= gpsdata.size();
+                tempgps.height /= gpsdata.size();
+                tempgps.utcgga /= gpsdata.size();
+                tempgps.utcgst /= gpsdata.size();
+                tempgps.latsig = sqrt(tempgps.latsig);
+                tempgps.lonsig = sqrt(tempgps.lonsig);
+                tempgps.heightsig = sqrt(tempgps.heightsig);
+            }
+            JSONObject jobject;
+            jobject.addElement("utcgga", tempgps.utcgga);
+            jobject.addElement("utcgst", tempgps.utcgst);
+            jobject.addElement("lat", tempgps.lat);
+            jobject.addElement("lon", tempgps.lon);
+            jobject.addElement("height", tempgps.height);
+            jobject.addElement("latsig", tempgps.latsig);
+            jobject.addElement("lonsig", tempgps.lonsig);
+            jobject.addElement("heightsig", tempgps.heightsig);
+            message = jobject.to_json_string();
+            log_write(agent->nodeName, agent->agentName, currentmjd(), "", "mean", message);
+            //                string result;
+            //                data_execute("rsync -auv " + ggapath + " ", result);
+            //                data_execute("rsync -auv " + gstpath, result);
+            log_move(agent->nodeName, agent->agentName);
+            gstmjd = currentmjd();
+            ggamjd = currentmjd();
+            et.reset();
         }
     }
 }
